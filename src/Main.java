1d5eb6b39cda56379c544891646d9443672c18f7
whilefor1010 <whilefor1010@gmail.com>
2022-10-10 14:25:06 +0300

    add print luckyNumber

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
+
+        int a = 1; // Начальное значение диапазона - "от"
+        int b = 10;// Конечное значение диапазона - "до"
+
+        int luckyNumber = a + (int) (Math.random() * b); // Генерация числа от 1 до 10
+
+        System.out.println("Your lucky number " + luckyNumber);
+
     }
 }
